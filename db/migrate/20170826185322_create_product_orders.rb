9b555b29426be4dc2aac0d10001244b60c240bfe
rcheng123 <robincheng999@gmail.com>
2017-08-26 15:46:39 -0400

    create product table

---
+++ db/migrate/20170826185322_create_product_orders.rb
@@ -0,0 +1,15 @@
+class CreateProductOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :product_orders do |t|
+      t.references :user, foreign_key: true
+      t.references :vendor, foreign_key: true
+      t.string :name, default: ""
+      t.decimal :price, default: 0
+      t.integer :quantity, default: 0
+      t.datetime :date
+      t.string :city, default: ""
+
+      t.timestamps
+    end
+  end
+end
