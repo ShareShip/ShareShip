40a4d786daa88444734e36c8756a6ca5633e982d
Dillion Verma <dillionverma@hotmail.com>
2017-08-27 09:24:27 -0400

    add faker to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+require 'Faker'
 
 User.create(email: 'admin@example.com', password: 'password', 
 			address_line_1: Faker::Address.street_address,
