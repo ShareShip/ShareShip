1296c7bc250223194831cf516ca612c8447b790c
Dillion Verma <dillionverma@hotmail.com>
2017-08-26 21:22:41 -0400

    remove merge conflict

--- db/migrate/20170826190554_add_address_to_users.rb
+++ db/migrate/20170826190554_add_address_to_users.rb
@@ -1,9 +1,5 @@
 class AddAddressToUsers < ActiveRecord::Migration[5.1]
   def change
-<<<<<<< HEAD
-    add_column :users, :address, :string
-    add_column :users, :city, :string
-=======
     add_column :users, :address_line_1, :string, default: ""
     add_column :users, :address_line_2, :string, default: ""
     add_column :users, :city, :string, default: ""
@@ -11,6 +7,5 @@ class AddAddressToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :zip, :string, default: ""
     add_column :users, :phone, :string, default: ""
 
->>>>>>> master
   end
 end
