9936e5b251f803b2ac54d4c7f214b01e2991751b
Dillion Verma <dillionverma@hotmail.com>
2017-08-27 06:36:26 -0400

    Allan (#8)
    * Front-end stuff
    
    * new user links
    
    * remove cache
    
    * Sign up/Sign in
    
    * Truck
    
    * code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'home#index'
-
-  get '/about' => 'home#about'
-
+  
+  get '/orders' => 'order#index'
+  get '/order/new' => 'order#new'
   get '/vendors' => 'vendor#index'
   get '/vendor/:name' => 'vendor#show'
-
   get '/payment/new' => 'payment#new'
-
-  get '/orders' => 'order#index'
-
+  
 end
