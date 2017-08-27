14fe22decfcd1d0747cf1e269819af8c4c7b3421
Robin Cheng <robincheng999@gmail.com>
2017-08-26 22:21:01 -0400

    Validate users (#7)
    * validate (#6)
    
    * more validation
    
    * more validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :vendor
-
   has_one :payment
+
+
+  validates :name, presence:true
+  validates :price, numericality: {greater_than: 0},
+  format: { :with => /\A\d+(?:\.\d{0,2})?\z/ } # 2 decimal max
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :city, presence:true
+  
 end
