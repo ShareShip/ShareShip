5e94224829350150b53bc9e9aae65391d2cf3921
Robin Cheng <robincheng999@gmail.com>
2017-08-26 20:06:13 -0400

    validate (#6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :payments_from, :class_name => 'Payment', :foreign_key => 'from_id'
   has_many :payments_to, :class_name => 'Payment', :foreign_key => 'to_id'
+
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence:true
+
 end
